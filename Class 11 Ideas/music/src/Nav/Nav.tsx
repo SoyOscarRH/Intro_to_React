351b848fc763303bea618973c8d84cd5158635df
SoyOscarRH <soyoscarrh@gmail.com>
2020-11-19 22:34:18 -0600

    Now we have something decent

--- Class 11 Ideas/music/src/Nav/Nav.tsx
+++ Class 11 Ideas/music/src/Nav/Nav.tsx
@@ -3,15 +3,15 @@ import styles from "./nav.module.css"
 
 import { BiSearchAlt, BiHomeAlt, BiLeftArrowAlt, BiMusic } from "react-icons/bi"
 
+const icons = [BiSearchAlt, BiHomeAlt, BiLeftArrowAlt, BiMusic]
 const Nav = () => {
   return (
     <aside>
-      <div className={styles.selected}>
-        <BiSearchAlt />
-      </div>
-      <BiHomeAlt />
-      <BiLeftArrowAlt />
-      <BiMusic />
+      {icons.map((Icon, index) => (
+        <div key={index} className={Icon === BiSearchAlt ? styles.selected : styles.unselected}>
+          <Icon />
+        </div>
+      ))}
     </aside>
   )
 }
--- Class 11 Ideas/music/src/Search/Search.tsx
+++ Class 11 Ideas/music/src/Search/Search.tsx
@@ -1,5 +1,7 @@
 import React, { useRef, useState } from "react"
 
+import { BiSearchAlt } from "react-icons/bi"
+
 import styles from "./search.module.css"
 
 const baseURL = "https://api.napster.com"
@@ -7,6 +9,7 @@ const key = "apikey=ZTk2YjY4MjMtMDAzYy00MTg4LWE2MjYtZDIzNjJmMmM0YTdm"
 
 const Main = () => {
   const [data, setData] = useState([])
+  const [message, setMessage] = useState("Let's search something ;)")
   const searchInput = useRef<HTMLInputElement>(null)
 
   const callAPI = async (queryLiteral: string) => {
@@ -17,38 +20,53 @@ const Main = () => {
     let json = await response.json()
     console.log(json)
 
+    setMessage("")
     setData(json.search.data.tracks)
+  }
+
+  const search = () => {
+    setData([])
+    setMessage("Loading ...")
+
+    const queryLiteral = searchInput.current?.value
+    callAPI(queryLiteral ?? "")
   }
 
   return (
     <>
       <form
         onSubmit={e => {
-          setData([])
-          const queryLiteral = searchInput.current?.value
-          callAPI(queryLiteral ?? "")
-
+          search()
           e.preventDefault()
           return false
         }}
       >
-        <input className={styles.search} ref={searchInput} />
+        <section className={styles.area}>
+          <div className={styles.iconWrapper} onClick={search}>
+            <BiSearchAlt />
+          </div>
+          <input className={styles.search} ref={searchInput} />
+        </section>
       </form>
 
-      <div className={styles.container}>
-        {data.map(({ albumName, name, previewURL }) => (
-          <article key={name + albumName} className={styles.tracks}>
-            <section className={styles.info}>
-              <h2>{name}</h2>
-              <p>{albumName}</p>
-            </section>
+      {message !== "" ? (
+        <h2 className={styles.message}>{message}</h2>
+      ) : (
+        <div className={styles.container}>
+          {data.map(({ albumName, name, previewURL }) => (
+            <article key={name + albumName} className={styles.track}>
+              <section className={styles.info}>
+                <p>{albumName}</p>
+                <h2>{name}</h2>
+              </section>
 
-            <audio controls>
-              <source src={previewURL} type="audio/mpeg" />
-            </audio>
-          </article>
-        ))}
-      </div>
+              <audio controls>
+                <source src={previewURL} type="audio/mpeg" />
+              </audio>
+            </article>
+          ))}
+        </div>
+      )}
     </>
   )
 }
